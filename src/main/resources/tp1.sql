47a4d0b285fa57797380bcbee94fe3efccd80bf2
Federico Aloi <federico.aloi@gmail.com>
2015-04-09 00:30:08 -0300

    Id autonumerico y unique solo en username

--- src/main/resources/tp1.sql
+++ src/main/resources/tp1.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS `tpepers`;
 CREATE DATABASE `tpepers`;
 CREATE TABLE `tpepers`.`usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `passwd` varchar(255) NOT NULL,
   `nombre` varchar(255) DEFAULT NULL,
@@ -9,10 +10,6 @@ CREATE TABLE `tpepers`.`usuario` (
   `nacimiento` date DEFAULT NULL,
   `cod_verif` varchar(255) DEFAULT NULL,
   `validado` tinyint(4) DEFAULT '0',
-  PRIMARY KEY (`username`),
-  UNIQUE KEY `nombre_UNIQUE` (`nombre`),
-  UNIQUE KEY `apellido_UNIQUE` (`apellido`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `validado_UNIQUE` (`validado`),
-  UNIQUE KEY `cod_verif_UNIQUE` (`cod_verif`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
