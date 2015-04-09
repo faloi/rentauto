14d96591933a3fbc21361279ec8b26886368a47d
Federico Aloi <federico.aloi@gmail.com>
2015-04-09 00:31:03 -0300

    Nombres de las columnas mas expresivos

--- src/main/resources/tp1.sql
+++ src/main/resources/tp1.sql
@@ -3,13 +3,13 @@ CREATE DATABASE `tpepers`;
 CREATE TABLE `tpepers`.`usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
-  `passwd` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `nombre` varchar(255) DEFAULT NULL,
   `apellido` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `nacimiento` date DEFAULT NULL,
-  `cod_verif` varchar(255) DEFAULT NULL,
-  `validado` tinyint(4) DEFAULT '0',
+  `codigo_validacion` varchar(255) DEFAULT NULL,
+  `is_validado` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uq_username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
